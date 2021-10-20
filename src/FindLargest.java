909d319a725c7b193a9348007e84a713561c1c8b
GabeOt <gabriel.otridge@ssfs.org>
2021-10-20 09:56:41 -0400

    completed the exercise

--- src/FindLargest.java
+++ src/FindLargest.java
@@ -5,6 +5,21 @@ public class FindLargest {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("What is the first integer?");
+        int num1 = scan.nextInt();
+        System.out.println("What is the second integer?");
+        int num2 = scan.nextInt();
+        if(num1>num2) {
+            System.out.println(num1 + " is larger than " + num2);
+        }
+        if(num1==num2) {
+            System.out.println(num1 + " is equal to " + num2);
+
+        }
+        if(num1<num2) {
+            System.out.println(num2 + " is greater than " + num1);
+        }
+
 
         // Prompt the user for two integer variables
 
